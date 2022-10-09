1c30ba7e6eaee938c615cb19f19bccd4f420b680
zyadhassan <zyad_hassan_2020@outlook.com>
2022-10-09 03:33:06 +0200

    add adc driver

---
+++ DRIVERS/adc.h
@@ -0,0 +1,22 @@
+/*
+ * adc.h
+ *
+ *  Created on: Oct 4, 2022
+ *      Author: User
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include "std_types.h"
+
+
+
+void ADC_init(void);
+uint16 ADC_readChannel(uint8 channel);
+
+
+
+
+
+#endif /* ADC_H_ */
